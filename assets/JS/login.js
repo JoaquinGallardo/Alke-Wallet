d807bed6c4641f824aeb73bbc895eba92aade6c7
JoaquinGallardo <jgallardoc91@gmail.com>
2024-03-08 20:32:08 -0300

    card added

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -11,7 +11,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         // Si las credenciales son correctas, redirige al usuario a otra página o muestra un mensaje de éxito
        
         alert("Inicio de sesión exitoso");
-        window.location.href="../../cuenta.html"
+        window.location.href="cuenta.html"
     } else {
         // Si las credenciales son incorrectas, muestra un mensaje de error
         document.getElementById('message text-bg-danger').textContent = 'Usuario o contraseña incorrectos';
