f2026a97de6364d04d24d4c40f43f99880d5f6d8
Joaquín Gallardo <jgallardoc91@gmail.com>
2024-03-11 19:36:52 -0300

    login2 modified

--- assets/JS/login2.js
+++ assets/JS/login2.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
-  
+
+
 $('#loginForm').submit(function(event) {
     event.preventDefault();
   
     // Obtener los valores de los campos
     let username = $('#username').val();
     let password = $('#password').val();
+
+    let h2Text = $('#alertBoot').text(); 
+
+    
   
     // Lógica de autenticación
     if (username === 'usuario' && password === 'contraseña') {
+      alert('Login exitoso');
       // Redirigir al usuario a otra página
-      window.location.href = "cuenta.html";
+      window.location.href = "cuenta.html"; 
     } 
     else {
       // Mostrar mensaje de error
-      $('#message-error').text('Usuario o contraseña incorrectos');
+      $('#alertBoot').removeClass('d-none');
+      $('#alertBoot').addClass('text-danger');
+      alert('Usuario o contraseña incorrectos');
+      console.log(h2Text);
     }
   });
 
