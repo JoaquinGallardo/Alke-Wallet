b175bb6b920b89d7a0df49c20d77f7920547eee8
JoaquinGallardo <jgallardoc91@gmail.com>
2024-03-12 10:47:18 -0300

    modified

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -1,19 +1,18 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+    event.preventDefault();  /* Evitar que el formulario se envíe automáticamente */
 
-    // Obtener los valores de los campos de entrada
+    /* Valores de los campos de entrada */
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    // Aquí puedes agregar tu lógica de autenticación, como enviar los datos a un servidor y recibir una respuesta
-    // Por ahora, simularemos una autenticación básica con valores de usuario y contraseña fijos
+    /* Lógica de autenticación */
     if (username === 'usuario' && password === 'contraseña') {
-        // Si las credenciales son correctas, redirige al usuario a otra página o muestra un mensaje de éxito
+         /* Si las credenciales son correctas, redirige al usuario a otra página y muestra un mensaje de éxito */
        
         alert("Inicio de sesión exitoso");
         window.location.href="cuenta.html"
     } else {
-        // Si las credenciales son incorrectas, muestra un mensaje de error
+        /* Mostrar un mensaje de error */
         document.getElementById('message text-bg-danger').textContent = 'Usuario o contraseña incorrectos';
         alert('Usuario o contraseña incorrectos');
     }
